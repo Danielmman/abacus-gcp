9b8c31992a3070ea536b7c6f7a5ba542f2de804b
Daniel <danielimmanj@gmail.com>
2024-04-21 16:18:42 +0530

    initial project

---
+++ abacus-gcp.js
@@ -0,0 +1,36 @@
+import express from "express";
+import { Storage } from "@google-cloud/storage";
+
+const app = express();
+const storage = new Storage({ keyFilename: "abacus-gcp.json" });
+const bucketName = "teachertrainingvidbucket";
+const port = 3000;
+
+app.post("/video-url", async (req, res) => {
+  try {
+    const fileName = req.query.fileName;
+    if (!fileName) {
+      return res.status(400).send("File name is required");
+    }
+
+    const options = {
+      version: "v4",
+      action: "read",
+      expires: Date.now() + 15 * 60 * 1000, // 15 minutes
+    };
+
+    const [url] = await storage
+      .bucket(bucketName)
+      .file(fileName)
+      .getSignedUrl(options);
+
+    res.send({ url });
+  } catch (error) {
+    console.error("Error generating signed URL:", error);
+    res.status(500).send("Failed to generate signed URL");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
